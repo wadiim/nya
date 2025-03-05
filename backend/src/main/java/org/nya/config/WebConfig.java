5a3857f3efcaa8e1a0e0ebf59b259496cce019d6
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2025-03-05 17:37:38 +0100

    Ohayo

---
+++ backend/src/main/java/org/nya/config/WebConfig.java
@@ -0,0 +1,11 @@
+package org.nya.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("org.nya")
+public class WebConfig {
+}
