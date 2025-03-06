879243842e474ae68ccfab144ed116b2e49e4c68
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2025-03-06 01:52:43 +0100

    Configure logging

--- backend/src/main/java/org/nya/controllers/HelloController.java
+++ backend/src/main/java/org/nya/controllers/HelloController.java
@@ -4,12 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.java.Log;
+
+@Log
 @RestController
 @RequestMapping("/api")
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
+        log.info("/api/hello endpoint called");
+
         return "Nya~!";
     }
 }
